027e249d4f0d6f3325b432964ac928aed326bb36
joshuaalbert <albertjoshua5@gmail.com>
2023-10-05 12:19:22 +0200

    add HelloWorldController

---
+++ backend/src/main/java/de/neuefische/backend/HelloWorldController.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello, world!";
+    }
+
+}
