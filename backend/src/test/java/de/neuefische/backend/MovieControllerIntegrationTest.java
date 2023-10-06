42f4abb24b87a4f0bc7e9b065e36ddfa969c76ae
joshuaalbert <albertjoshua5@gmail.com>
2023-10-06 13:01:05 +0200

    add a first integration test for MovieController class

--- backend/src/test/java/de/neuefische/backend/MovieControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/MovieControllerIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
