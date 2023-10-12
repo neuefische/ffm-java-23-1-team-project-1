e2724e4e40bbd9070f7276a93fde21e3c0c93053
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-10-12 18:54:21 +0200

    - logo zum Header hinzugefügt - nav zum header mit Links zur /movies - logo und h1 führen zur "/" - hover effekt auf a-tag im header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,7 +4,18 @@ import {Link} from "react-router-dom";
 export default function Header() {
     return (
         <header>
-            <Link to={"/movies"}><h1>FrameFiesta</h1></Link>
+            <div>
+                <Link to={"/"}>
+                    <img src={"https://banner2.cleanpng.com/20190730/shy/kisspng-photographic-film-movie-camera-cinema-website-and-mobile-application-development-service-5d3fc924ce3b33.8538265315644613488447.jpg"} alt={"framefiesta logo"}/>
+                    <h1>FrameFiesta</h1>
+                </Link>
+            </div>
+            <nav>
+                <ul>
+                    <li><Link to={"/movies"}>Alle Filme</Link></li>
+                    <li><Link to={"/movies"}>Favoriten</Link></li>
+                </ul>
+            </nav>
         </header>
     );
 }
